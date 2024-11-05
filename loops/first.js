b61ece54a6be040e273c4867c3158266a81ba088
sakshi9070 <dukaresakshi329@gmail.com>
2024-11-05 21:30:53 +0530

    first commit

---
+++ loops/first.js
@@ -0,0 +1,90 @@
+//loops
+
+// for(let i=1; i<=100; i++){
+//     console.log("sakshi");
+// }
+
+// console.log("loops has ended");
+
+
+// calculate sum of n number
+
+// let sum=0;
+// let n=100;
+
+// for(let i=1; i<=n; i++){
+//       sum=sum+i;
+//     }
+//     console.log("sum :",sum);
+
+    // infinite loops : A loop is never end
+
+    // for(let i=1; i>=n; i++) 
+
+
+    // while loop
+
+//     let i=1;
+//    while(i<=5){
+//         console.log("mahi");
+//         i++;
+//    }
+
+
+   // do while 
+
+//    let i=1;
+//    do{
+//     console.log("saku");
+//     i++;
+//    } while(i<=10);
+
+
+// for-of loop
+
+// let str="saku";
+// for(let i of str){
+//   console.log("i=",i);
+// }
+
+
+// for-in loop -> is return keys
+
+let student={
+    name:"sakshi",
+    age:21,
+    cgpa:8.68,
+    isPass:true
+};
+
+for(let key in student){
+    console.log("key=",key,"value",student[key]);
+}
+ 
+// print all sum or odd num
+
+//  let n=100;
+// for(let i=0; i<=n; i++){
+    
+//  if(i%2 !==0){
+//     console.log(i);
+//  }
+// }
+
+
+//que
+
+// let gameNum=25;
+
+//  let result=prompt("Guess the gaame number:");
+ 
+//  while(result!=gameNum){
+//     result=prompt(" you Enter wrong number, gues again: ");
+//  }  
+//  console.log("congeratulayions, you enter right number:");
+
+
+ //strings
+
+ let name="saku";
+ console.log(name);
